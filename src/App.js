6611558a9bd3214a73752bd7d578a0939b91a6ad
N-sing <nksingh.niteshsingh@gmail.com>
2022-10-11 14:39:04 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Dogbreed from './Dogbreed';
+
+function App() {
+  return (
+    <div className="App">
+      <Dogbreed/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CustomSearch.js
@@ -0,0 +1,65 @@
+import React,{useState} from 'react';
+//import './Header.css'
+import '../Header.css'
+
+function CustomSearch({breeds,breedinfo}) {
+    const [breedName,setBreedName] = useState("");
+    const [imgCount,setImgCount] = useState("");
+    
+
+
+    const handleChang=(e)=>{
+        setBreedName(e.target.value);
+    }
+    const inputNumber=(e)=>{
+        setImgCount(e.target.value);
+    }
+
+
+    const handleData = (e) =>{
+        e.preventDefault();
+        if(breedName==""){
+            alert("Plz Select Breed Name");
+        }else if(imgCount<1 || imgCount>30){
+            alert("Plz Enter A valid numver")
+        }else{
+            
+
+
+            breedinfo(breedName,imgCount);
+            setBreedName("");
+            setImgCount("");
+            
+
+        }
+    }
+
+    
+    return (
+        <div>
+            <form onSubmit={handleData} >
+                <div className="dlist">
+                    <select className="dropdownlist" onChange={handleChang} value={breedName} name="" id="">
+                        {
+                            breeds.map(
+                                breed=>(
+                                    <option className="list-item"   >{breed}</option>
+                                )
+                            )
+                            
+                        }
+                    </select>
+                </div>
+                    <div className="inpt">
+                        <input id="inpnumber" type="number" value={imgCount}  onChange={inputNumber} placeholder="Number of Images" />
+                    </div>
+                    <div className="getImg">
+                        <button className="getbtn" type="submit">Get Images</button>
+                    </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default CustomSearch
